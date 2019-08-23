bc8fe597bc87b91748881775b11801d650c6f428
Steve <mittosteve@gmail.com>
2019-08-23 11:59:45 +0300

    This commit adds nav , git and git-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GitComponent } from './git/git.component';
+import { GitSearchComponent } from './git-search/git-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GitComponent,
+    GitSearchComponent
   ],
   imports: [
     BrowserModule,
