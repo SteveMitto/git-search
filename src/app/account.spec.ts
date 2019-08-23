0c5daa1e321da80545dd69c7178b90354a915729
Steve <mittosteve@gmail.com>
2019-08-23 17:52:47 +0300

    This commit creates the account class for the blue print

---
+++ src/app/account.spec.ts
@@ -0,0 +1,7 @@
+import { Accout } from './accout';
+
+describe('Accout', () => {
+  it('should create an instance', () => {
+    expect(new Accout()).toBeTruthy();
+  });
+});
---
+++ src/app/account.ts
@@ -0,0 +1,5 @@
+export class Account {
+
+  constructor(public name:string,public avatarUrl:string,public public_repos:string, public repos_url:string,public followers:string, public following:string){}
+
+}
