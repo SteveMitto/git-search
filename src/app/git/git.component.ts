3d8a99f81c58f36c3cb1255e813b264727d3519b
Steve <mittosteve@gmail.com>
2019-08-23 17:54:56 +0300

    This commit manages the api link

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AccountHttpService } from '../service/account-http.service';
+import { Account} from '../account';
 @Component({
   selector: 'app-git',
   templateUrl: './git.component.html',
   styleUrls: ['./git.component.css']
 })
 export class GitComponent implements OnInit {
+  users:Account;
+  constructor(private accountService:AccountHttpService) { }
+  // console.log(user)
 
   sendName(search){
-    console.log(search)
+    this.accountService.searchName(search).then(
+      (results)=>{
+      this.users= this.accountService.user
+      console.log(this.users)
+      },
+      (error)=>{
+        console.log('error')
+      }
+    )
   }
-  constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/service/account-http.service.ts
+++ src/app/service/account-http.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient }  from '@angular/common/http';
+import { Account } from '../account';
+import { environment } from '../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class AccountHttpService {
+  user:Account;
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  searchName(search){
+    interface ApiResponse{
+      avatar_url:string;
+      login:string;
+      public_repos:string;
+      repos_url:string;
+      followers:string;
+      following:string;
+    }
+    let apiUrl="https:api.github.com/users/"+search+"?access_token="+environment.APIKEY;
+
+    let promise =new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>(apiUrl).toPromise().then(
+        (results)=>{
+          this.user =new Account(results.login,results.avatar_url,results.public_repos,results.repos_url,results.followers,results.following)
+          console.log(this.user)
+          // this.user.name=results.name
+          resolve()
+        },
+        (error)=>{
+            console.log('error')
+          reject()
+        })
+    })
+    return promise
+  }
 }
