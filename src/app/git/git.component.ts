b30598fb7ed2ba77db3e142f81d1153aa14da6d8
Steve <mittosteve@gmail.com>
2019-08-23 12:28:32 +0300

    This commit allows recieving of users input from the parrent

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitComponent implements OnInit {
 
+  sendName(search){
+    console.log(search)
+  }
   constructor() { }
 
   ngOnInit() {
