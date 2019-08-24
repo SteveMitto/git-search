793b3a2fbe6af924f2cb27da4dc3d4ffb3a845e9
Steve <mittosteve@gmail.com>
2019-08-24 09:50:41 +0300

    This commit manages the display of users data

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountHttpService } from '../service/account-http.service';
 import { Account} from '../account';
+import { Repository } from '../repository'
 @Component({
   selector: 'app-git',
   templateUrl: './git.component.html',
@@ -8,8 +9,10 @@ import { Account} from '../account';
 })
 export class GitComponent implements OnInit {
   users:Account;
+  user
   constructor(private accountService:AccountHttpService) { }
   // console.log(user)
+
 
   sendName(search){
     this.accountService.searchName(search).then(
@@ -20,9 +23,11 @@ export class GitComponent implements OnInit {
       (error)=>{
         console.log('error')
       }
-    )
+    );
   }
+
   ngOnInit() {
+    this.sendName("SteveMitto")
   }
 
 }
