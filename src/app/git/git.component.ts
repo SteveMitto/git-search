a2f8e204a13228c809fb8653db0946f41f31fca0
Steve <mittosteve@gmail.com>
2019-08-25 18:32:32 +0300

    This commit imports the repos url class into the git .component class

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -3,6 +3,8 @@ import { AccountHttpService } from '../service/account-http.service';
 import { RepositoryHttpService } from '../service/repository-http.service';
 import { Account} from '../account';
 import { Repository } from '../repository'
+import { RepoUrl } from '../repo-url'
+
 @Component({
   selector: 'app-git',
   templateUrl: './git.component.html',
@@ -11,21 +13,21 @@ import { Repository } from '../repository'
 export class GitComponent implements OnInit {
   users:Account;
   repos:Repository[]=[];
-  repos_url:[]=[];
+  repos_url:RepoUrl[]=[];
+
   constructor(private accountService:AccountHttpService,private repoService:RepositoryHttpService) { }
   // console.log(user)
 
 
   getRepository(search){
-    this.repoService.getRep(search).then(
-      (reults)=>{
-        this.repos=this.repoService.repos
-        debugger
-        console.log(this.repos)
-          // console.log("success")
+    this.repoService.getRepository(search).then(
+      (results)=>{
+        this.repos = this.repoService.repos
+        this.repos_url = this.repoService.repos_url
+        console.log(this.repos_url)
       },
       (error)=>{
-        console.log("kubwa sana")
+        console.log("could not fetch repo");
       }
     )
   }
--- src/app/repo-url.ts
+++ src/app/repo-url.ts
@@ -1,2 +1,3 @@
 export class RepoUrl {
+  constructor(public repoUrl:string){}
 }
