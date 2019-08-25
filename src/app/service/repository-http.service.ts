737d0a6b86bfad83e9ac382682727f9d709e3d67
Steve <mittosteve@gmail.com>
2019-08-25 18:32:56 +0300

    This commit allows fetching of users repos url from the web

--- src/app/service/repository-http.service.ts
+++ src/app/service/repository-http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Repository } from '../repository';
+import { RepoUrl } from '../repo-url'
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -7,31 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RepositoryHttpService {
 
-repos:Repository[]=[];
-repos_url:[]=[];
+  repos: Repository[] = [];
+  repos_url: RepoUrl[] = [];
 
-  constructor(public http:HttpClient) { }
+  constructor(public http: HttpClient) { }
 
- getRep(username){
-   let apiLink="https://api.github.com/users/"+username+"/repos"
-  let promise =new Promise((resolve,reject)=>{
-    this.http.get<[]>(apiLink).toPromise().then(
-    (results)=>{
-      this.repos=[];
-      this.repos_url=[];
-      for(let i =0;i<results.length;i++){
-      // console.log(results)
-      this.repos.push(results[i]["name"])
-      this.repos_url.push(results[i]["url"])
-    }
-    console.log(this.repos)
-    console.log(this.repos_url)
-
-    },
-    (error)=>{
-      console.log("failed")
+  getRepository(username) {
+    let apiLink = "https://api.github.com/users/" + username + "/repos"
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<[]>(apiLink).toPromise().then(
+        (results) => {
+          this.repos = [];
+          this.repos_url = [];
+            for (let i = 0; i < results.length; i++) {
+              // console.log(results)
+              this.repos.push(results[i]["name"])
+              this.repos_url.push(results[i]["url"])
+            }
+          // console.log(this.repos)
+          // console.log(this.repos_url)
+          resolve()
+        },
+        (error) => {
+          console.log("failed")
+          reject(error)
+        })
     })
-  })
-  return promise
+    return promise
   }
 }
